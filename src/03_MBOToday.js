2d1d6b7b6948b3d2683ff59feefe4306533f7851
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-05-11 18:19:00 +0900

    #4 v3_時間分割モード追加

--- src/03_MBOToday.js
+++ src/03_MBOToday.js
@@ -2,40 +2,90 @@
 
 function doneMBO(e){
     const eCheck = e['value'];
-    const doneCheck = todaySheet.getRange("H12").getValue();
-    const errorCheck = todaySheet.getRange("N12").getValue();
+    const doneCheck = todaySheet.getRange("G12").getValue();
+    const errorCheck = todaySheet.getRange("L12").getValue();
     const kahiCheck = todaySheet.getRange("A2").getValue() === "可";
     const runFlag = eCheck && doneCheck && errorCheck && kahiCheck;
-    const todayRow = todaySheet.getRange("Q2").getValue();
-    let yyyy = todaySheet.getRange("K2").getValue();
-    let mm = todaySheet.getRange("L2").getValue()-1;
-    let dd = todaySheet.getRange("M2").getValue();
+    const mode1 = eCheck === "時間分割モード";
+    const mode2 = eCheck === "直接入力モード";
+    const todayRow = todaySheet.getRange("O2").getValue();
+    let yyyy = todaySheet.getRange("I2").getValue();
+    let mm = todaySheet.getRange("J2").getValue()-1;
+    let dd = todaySheet.getRange("K2").getValue();
     let yyyymmdd = new Date(yyyy,mm,dd);
-    if(runFlag){
-        todaySheet.getRange("E13").copyTo(mboSheet.getRange("BK"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("E14").copyTo(mboSheet.getRange("BL"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("H14").copyTo(mboSheet.getRange("H"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("C15:F15").copyTo(mboSheet.getRange("K"+todayRow+":N"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("C16:F16").copyTo(mboSheet.getRange("R"+todayRow+":U"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("C17:F17").copyTo(mboSheet.getRange("Y"+todayRow+":AB"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("C18:F18").copyTo(mboSheet.getRange("AF"+todayRow+":AI"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("H15").copyTo(mboSheet.getRange("P"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("H16").copyTo(mboSheet.getRange("W"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("H17").copyTo(mboSheet.getRange("AD"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("H18").copyTo(mboSheet.getRange("AK"+todayRow),{contentsOnly:true});
-        todaySheet.getRange("C15:E18").clearContent();
-        todaySheet.getRange("H14:H18").clearContent();
-        todaySheet.getRange("F15:F18").setValue(false);
-        todaySheet.getRange("E13").setValue("起床");
-        todaySheet.getRange("E14").setValue("就寝");
-        yyyymmdd = new Date(yyyymmdd.getFullYear(), yyyymmdd.getMonth(), yyyymmdd.getDate()+1);
-        yyyy = yyyymmdd.getFullYear();
-        mm = yyyymmdd.getMonth()+1;
-        dd = yyyymmdd.getDate();
-        todaySheet.getRange("K2").setValue(yyyy);
-        todaySheet.getRange("L2").setValue(mm);
-        todaySheet.getRange("M2").setValue(dd);
-        todaySheet.getRange("Q1").setValue(Utilities.formatDate(new Date(),'Asia/Tokyo','yyyy/MM/dd HH:mm:ss'));
+
+    if(mode1 || mode2){
+        if(mode1){
+            todaySheet.showRows(16,3); // W時間分割行表示
+            todaySheet.showRows(20,3); // F時間分割行表示
+            todaySheet.showRows(24,3); // L時間分割行表示
+            todaySheet.showRows(28,3); // E時間分割行表示
+            todaySheet.getRange("E15").setBackgroundRGB(255,242,204); // W背景色（編集禁止）
+            todaySheet.getRange("E19").setBackgroundRGB(255,242,204); // F背景色（編集禁止）
+            todaySheet.getRange("E23").setBackgroundRGB(255,242,204); // L背景色（編集禁止）
+            todaySheet.getRange("E27").setBackgroundRGB(255,242,204); // E背景色（編集禁止）
+            todaySheet.getRange("E15").setFormula('=sumifs($J$15:$J$30,$I$15:$I$30,mid($B15,1,1)&"(工数)")'); // W工数集計数式貼付
+            todaySheet.getRange("E19").setFormula('=sumifs($J$15:$J$30,$I$15:$I$30,mid($B19,1,1)&"(工数)")'); // F工数集計数式貼付
+            todaySheet.getRange("E23").setFormula('=sumifs($J$15:$J$30,$I$15:$I$30,mid($B23,1,1)&"(工数)")'); // L工数集計数式貼付
+            todaySheet.getRange("E27").setFormula('=sumifs($J$15:$J$30,$I$15:$I$30,mid($B27,1,1)&"(工数)")'); // E工数集計数式貼付
+        }else if(mode2){
+            todaySheet.hideRows(16,3); // W時間分割行非表示
+            todaySheet.hideRows(20,3); // F時間分割行非表示
+            todaySheet.hideRows(24,3); // L時間分割行非表示
+            todaySheet.hideRows(28,3); // E時間分割行非表示
+            todaySheet.getRange("E15").setBackgroundRGB(255,255,255); // W背景色（編集可能）
+            todaySheet.getRange("E19").setBackgroundRGB(255,255,255); // F背景色（編集可能）
+            todaySheet.getRange("E23").setBackgroundRGB(255,255,255); // L背景色（編集可能）
+            todaySheet.getRange("E27").setBackgroundRGB(255,255,255); // E背景色（編集可能）
+            todaySheet.getRange("E15").clearContent(); // W工数集計数式クリア
+            todaySheet.getRange("E19").clearContent(); // F工数集計数式クリア
+            todaySheet.getRange("E23").clearContent(); // L工数集計数式クリア
+            todaySheet.getRange("E27").clearContent(); // E工数集計数式クリア
+        }
     }
-    todaySheet.getRange("H12").setValue(false);
+
+    if(runFlag){
+        todaySheet.getRange("G13").copyTo(mboSheet.getRange("BK"+todayRow),{contentsOnly:true}); // 起床
+        todaySheet.getRange("G14").copyTo(mboSheet.getRange("BL"+todayRow),{contentsOnly:true}); // 就寝
+        todaySheet.getRange("E14").copyTo(mboSheet.getRange("H"+todayRow),{contentsOnly:true}); // 仮眠
+        todaySheet.getRange("C15:E15").copyTo(mboSheet.getRange("K"+todayRow+":M"+todayRow),{contentsOnly:true}); // W目標・振返・工数
+        todaySheet.getRange("C19:E19").copyTo(mboSheet.getRange("R"+todayRow+":T"+todayRow),{contentsOnly:true}); // F目標・振返・工数
+        todaySheet.getRange("C23:E23").copyTo(mboSheet.getRange("Y"+todayRow+":AA"+todayRow),{contentsOnly:true}); // L目標・振返・工数
+        todaySheet.getRange("C27:E27").copyTo(mboSheet.getRange("AF"+todayRow+":AH"+todayRow),{contentsOnly:true}); // E目標・振返・工数
+        todaySheet.getRange("F15").copyTo(mboSheet.getRange("P"+todayRow),{contentsOnly:true}); // W相殺
+        todaySheet.getRange("F19").copyTo(mboSheet.getRange("W"+todayRow),{contentsOnly:true}); // F相殺
+        todaySheet.getRange("F23").copyTo(mboSheet.getRange("AD"+todayRow),{contentsOnly:true}); // L相殺
+        todaySheet.getRange("F27").copyTo(mboSheet.getRange("AK"+todayRow),{contentsOnly:true}); // E相殺
+        todaySheet.getRange("G15").copyTo(mboSheet.getRange("O"+todayRow),{contentsOnly:true}); // W評価
+        todaySheet.getRange("G19").copyTo(mboSheet.getRange("V"+todayRow),{contentsOnly:true}); // F評価
+        todaySheet.getRange("G23").copyTo(mboSheet.getRange("AC"+todayRow),{contentsOnly:true}); // L評価
+        todaySheet.getRange("G27").copyTo(mboSheet.getRange("AJ"+todayRow),{contentsOnly:true}); // E評価
+        todaySheet.getRange("C15:E15").clearContent(); // W目標・振返・工数クリア
+        todaySheet.getRange("C19:E19").clearContent(); // F目標・振返・工数クリア
+        todaySheet.getRange("C23:E23").clearContent(); // L目標・振返・工数クリア
+        todaySheet.getRange("C27:E27").clearContent(); // E目標・振返・工数クリア
+        todaySheet.getRange("F15").setValue(false); // W相殺クリア
+        todaySheet.getRange("F19").setValue(false); // W相殺クリア
+        todaySheet.getRange("F23").setValue(false); // W相殺クリア
+        todaySheet.getRange("F27").setValue(false); // W相殺クリア
+        todaySheet.getRange("G13").setValue("起床"); // 起床クリア
+        todaySheet.getRange("G14").setValue("就寝"); // 就寝クリア
+        todaySheet.getRange("G15").clearContent(); // W評価クリア
+        todaySheet.getRange("G19").clearContent(); // F評価クリア
+        todaySheet.getRange("G23").clearContent(); // L評価クリア
+        todaySheet.getRange("G27").clearContent(); // E評価クリア
+        todaySheet.getRange("D16:E18").clearContent(); // W時間分割クリア
+        todaySheet.getRange("D20:E22").clearContent(); // F時間分割クリア
+        todaySheet.getRange("D24:E26").clearContent(); // L時間分割クリア
+        todaySheet.getRange("D28:E30").clearContent(); // E時間分割クリア
+        yyyymmdd = new Date(yyyymmdd.getFullYear(), yyyymmdd.getMonth(), yyyymmdd.getDate()+1); // 日付スライド
+        yyyy = yyyymmdd.getFullYear(); // yyyy再取得
+        mm = yyyymmdd.getMonth()+1; // mm再取得
+        dd = yyyymmdd.getDate(); // dd再取得
+        todaySheet.getRange("I2").setValue(yyyy); // yyyy再貼付
+        todaySheet.getRange("J2").setValue(mm); // mm再貼付
+        todaySheet.getRange("K2").setValue(dd); // dd再貼付
+        todaySheet.getRange("O1").setValue(Utilities.formatDate(new Date(),'Asia/Tokyo','yyyy/MM/dd HH:mm:ss')); // 処理日時
+    }
+    todaySheet.getRange("G12").setValue(false); // Doneチェックボックス初期化
 }

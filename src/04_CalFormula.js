14433cb6967efdc13a7f001e2498d7e64e4a35f5
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-07 23:35:43 +0900

    #10 空き行ができた場合のみ発動

--- src/04_CalFormula.js
+++ src/04_CalFormula.js
@@ -1,4 +1,7 @@
 // Cal数式
 function calFormula(){
-    calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=counta(F'+(beginRow_Cal+1)+':NF'+(beginRow_Cal+1)+')');
+    const nullFormula = calSheet.getRange(beginRow_Cal,1).getValue() !== 0;
+    if(nullFormula){
+        calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=counta(F'+(beginRow_Cal+1)+':NF'+(beginRow_Cal+1)+')');
+    }
 }

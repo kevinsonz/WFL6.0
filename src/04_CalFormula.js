b3c94f4c4b3f56d573740632741d50a985c37e70
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-08 22:13:18 +0900

    #10 親を常に表示＋1行に1項目である前提

--- src/04_CalFormula.js
+++ src/04_CalFormula.js
@@ -2,6 +2,6 @@
 function calFormula(){
     const nullFormula = calSheet.getRange(beginRow_Cal,1).getValue() !== 0;
     if(nullFormula){
-        calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=counta(H'+(beginRow_Cal+1)+':NI'+(beginRow_Cal+1)+')');
+        calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=if(counta(E'+(beginRow_Cal+1)+':G'+(beginRow_Cal+1)+')=1,if(G'+(beginRow_Cal+1)+'<>"",counta(H'+(beginRow_Cal+1)+':NI'+(beginRow_Cal+1)+'),1),0)');
     }
 }

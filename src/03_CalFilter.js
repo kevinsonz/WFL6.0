889853ab8dcd5a03356506e70084ea4415759bf6
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-08 18:04:26 +0900

    #10 E廃止＋完了チェック追加

--- src/03_CalFilter.js
+++ src/03_CalFilter.js
@@ -1,21 +1,32 @@
 // Calフィルター
 function calFilter(e){
     const filCk = calSheet.getRange('A7').getValue(); // チェック状態
-    const eCell = (e['range'].getRow() === 7 && e['range'].getColumn() === 1); // チェック
-    if(eCell){
+    const cmpCk = calSheet.getRange('B7').getValue(); // チェック状態
+    const eCell1 = (e['range'].getRow() === 7 && e['range'].getColumn() === 1); // チェック
+    const eCell2 = (e['range'].getRow() === 7 && e['range'].getColumn() === 2); // チェック
+    if(eCell1 || eCell2){
         let filterMBO = calSheet.getFilter();
         if(filterMBO !== null){
             calSheet.getFilter().remove();
         }
-        let rule = SpreadsheetApp.newFilterCriteria()
+        let rule1 = SpreadsheetApp.newFilterCriteria()
                 .setHiddenValues([])
                 .build();
         if(filCk){
-            rule = SpreadsheetApp.newFilterCriteria()
+            rule1 = SpreadsheetApp.newFilterCriteria()
                 .setHiddenValues([0])
                 .build();
         }
-        calSheet.getRange(beginRow_Cal,1,endRow_Cal-beginRow_Cal+1,1).createFilter()
-            .setColumnFilterCriteria(1,rule);
+        let rule2 = SpreadsheetApp.newFilterCriteria()
+                .setHiddenValues([])
+                .build();
+        if(cmpCk){
+            rule2 = SpreadsheetApp.newFilterCriteria()
+                .setHiddenValues(['true'])
+                .build();
+        }
+        calSheet.getRange(beginRow_Cal,1,endRow_Cal-beginRow_Cal+1,2).createFilter()
+            .setColumnFilterCriteria(1,rule1)
+            .setColumnFilterCriteria(2,rule2);
     }
 }

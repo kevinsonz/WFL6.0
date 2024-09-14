f8eb0f4b00f8c68d6dbbcbab17f5c69c7b6f34b4
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-14 22:14:18 +0900

    #10 □削除機能実装

--- src/04_CalFormula.js
+++ src/04_CalFormula.js
@@ -5,6 +5,6 @@ function calFormula(){
         calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=if(counta(E'+(beginRow_Cal+1)+':G'+(beginRow_Cal+1)+')=1,if(G'+(beginRow_Cal+1)+'<>"",counta(K'+(beginRow_Cal+1)+':NL'+(beginRow_Cal+1)+'),1),0)');
         calSheet.getRange('H'+(beginRow_Cal+1)+':H').setFormula('=iferror(if(countifs($K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',H$11)=1,match(H$11,$K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',0),if(countifs($K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',H$11)>1,-1,0)),-1)');
         calSheet.getRange('I'+(beginRow_Cal+1)+':I').setFormula('=iferror(if(countifs($K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',I$11)=1,match(I$11,$K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',0),if(countifs($K'+(beginRow_Cal+1)+':$NL'+(beginRow_Cal+1)+',I$11)>1,-1,0)),-1)');
-        calSheet.getRange('J'+(beginRow_Cal+1)+':J').setFormula('=and(H'+(beginRow_Cal+1)+'>0,I'+(beginRow_Cal+1)+'>0,H'+(beginRow_Cal+1)+'<I'+(beginRow_Cal+1)+')');
+        calSheet.getRange('J'+(beginRow_Cal+1)+':J').setFormula('=if(and(H'+(beginRow_Cal+1)+'>0,I'+(beginRow_Cal+1)+'>0,H'+(beginRow_Cal+1)+'<I'+(beginRow_Cal+1)+'),"A",if(and(H'+(beginRow_Cal+1)+'>0,I'+(beginRow_Cal+1)+'>0,H'+(beginRow_Cal+1)+'>I'+(beginRow_Cal+1)+'),"B","-"))');
     }
 }

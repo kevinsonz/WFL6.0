09ac0d17c28123a5f8817256c3eca3d230a8e8cf
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-14 15:06:40 +0900

    #10 バグ改修

--- src/05_CalBatch.js
+++ src/05_CalBatch.js
@@ -1,21 +1,22 @@
 // Cal範囲指定
 function calBatch(){
     const batNumber = calSheet.getRange(beginRow_Cal,batFlagCol_Cal).getValue(); // チェック状態
-    const batStartRow = getRange(beginRow_Cal+batNumber,batStartCol_Cal).getValue(); // →列情報
-    const batGoalRow = getRange(beginRow_Cal+batNumber,batGoalCol_Cal).getValue(); // ←列情報
+    const batStartRow = calSheet.getRange(beginRow_Cal+batNumber,batStartCol_Cal).getValue(); // →列情報
+    const batGoalRow = calSheet.getRange(beginRow_Cal+batNumber,batGoalCol_Cal).getValue(); // ←列情報
+
     if(batNumber>0){
-        let pjRowData = [];
-        pjRowData = getRange((beginRow_Cal + batNumber),beginCol_Cal).getValues().flat();
-        for(i=0;i<pjRowData.length;i++){
+        let pjRowData = calSheet.getRange(beginRow_Cal+batNumber,beginCol_Cal,1,366).getValues();
+        for(i=0;i<pjRowData[0].length;i++){
             if((batStartRow-1)<=i && i<=(batGoalRow-1)){
-                if(pjRowData[i]=''){
-                    pjRowData.splice(i,1,'□');
+                if(pjRowData[0][i]===''||pjRowData[0][i]==='→'||pjRowData[0][i]==='←'){
+                    pjRowData[0].splice(i,1,'□');
+                }
+            }else{
+                if(pjRowData[0][i]==='□'){
+                    pjRowData[0].splice(i,1,'');
                 }
             }
-            if(pjRowData[i]='□'){
-                pjRowData.splice(i,1,'');
-            }
         }
-        getRange((beginRow_Cal + batNumber),beginCol_Cal).setValues(pjRowData);
+        calSheet.getRange(beginRow_Cal+batNumber,beginCol_Cal,1,366).setValues(pjRowData);
     }
 }

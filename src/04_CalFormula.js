b96cd9949c1c83bb098e07e66cce276a349698ba
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-16 23:38:50 +0900

    #10 空白行は常に表示

--- src/04_CalFormula.js
+++ src/04_CalFormula.js
@@ -2,7 +2,7 @@
 function calFormula(){
     const nullFormula = calSheet.getRange(beginRow_Cal,1).getValue() !== 0;
     if(nullFormula){
-        calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=iferror(if($NM'+(beginRow_Cal+1)+'=1,ifs(and($D$5=false,$D$6=false),1,and($D$5=true,$D$6=false),$NV'+(beginRow_Cal+1)+',and($D$5=false,$D$6=true),$NW'+(beginRow_Cal+1)+',and($D$5=true,$D$6=true),$NV'+(beginRow_Cal+1)+'*$NW'+(beginRow_Cal+1)+'),0),0)');
+        calSheet.getRange('A'+(beginRow_Cal+1)+':A').setFormula('=iferror(if(counta($E'+(beginRow_Cal+1)+':$G'+(beginRow_Cal+1)+')=0,1,if($NM'+(beginRow_Cal+1)+'=1,ifs(and($D$5=false,$D$6=false),1,and($D$5=true,$D$6=false),$NV'+(beginRow_Cal+1)+',and($D$5=false,$D$6=true),$NW'+(beginRow_Cal+1)+',and($D$5=true,$D$6=true),$NV'+(beginRow_Cal+1)+'*$NW'+(beginRow_Cal+1)+'),0)),0)');
         calSheet.getRange('NJ'+(beginRow_Cal+1)+':NJ').setFormula('=iferror(if(countifs($H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',NJ$11)=1,match(NJ$11,$H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',0),if(countifs($H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',NJ$11)>1,-1,0)),-1)');
         calSheet.getRange('NK'+(beginRow_Cal+1)+':NK').setFormula('=iferror(if(countifs($H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',NK$11)=1,match(NK$11,$H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',0),if(countifs($H'+(beginRow_Cal+1)+':$NI'+(beginRow_Cal+1)+',NK$11)>1,-1,0)),-1)');
         calSheet.getRange('NL'+(beginRow_Cal+1)+':NL').setFormula('=if(and(NJ'+(beginRow_Cal+1)+'>0,NK'+(beginRow_Cal+1)+'>0,NJ'+(beginRow_Cal+1)+'<NK'+(beginRow_Cal+1)+'),"A",if(and(NJ'+(beginRow_Cal+1)+'>0,NK'+(beginRow_Cal+1)+'>0,NJ'+(beginRow_Cal+1)+'>NK'+(beginRow_Cal+1)+'),"B","-"))');

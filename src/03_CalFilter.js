46893797920df2abbc5a5379e4384d4211c46f3c
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2024-09-16 23:22:57 +0900

    #10 今ココ・活性絞込の階層対応

--- src/03_CalFilter.js
+++ src/03_CalFilter.js
@@ -12,21 +12,12 @@ function calFilter(e){
         let rule1 = SpreadsheetApp.newFilterCriteria()
                 .setHiddenValues([])
                 .build();
-        if(filCk){
+        if(filCk || cmpCk){
             rule1 = SpreadsheetApp.newFilterCriteria()
                 .setHiddenValues([0])
                 .build();
         }
-        let rule2 = SpreadsheetApp.newFilterCriteria()
-                .setHiddenValues([])
-                .build();
-        if(cmpCk){
-            rule2 = SpreadsheetApp.newFilterCriteria()
-                .setHiddenValues(['true'])
-                .build();
-        }
-        calSheet.getRange(beginRow_Cal,1,endRow_Cal-beginRow_Cal+1,2).createFilter()
-            .setColumnFilterCriteria(1,rule1)
-            .setColumnFilterCriteria(2,rule2);
+        calSheet.getRange(beginRow_Cal,1,endRow_Cal-beginRow_Cal+1,1).createFilter()
+            .setColumnFilterCriteria(1,rule1);
     }
 }

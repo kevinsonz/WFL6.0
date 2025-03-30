242c01b8ebccff7674cb819deb317f2747d891e3
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2025-03-31 00:00:05 +0900

    #11 新GTD向けフィルタ機能作成

--- src/01_variables.js
+++ src/01_variables.js
@@ -7,18 +7,25 @@ const consoleSheet = wflFile.getSheetByName('console');
 const kakoSheetName = '過去ログ'; // 本体ファイル
 const kakoSheetMain = wflFile.getSheetByName(kakoSheetName); // 本体ファイル
 const kakoFile = SpreadsheetApp.openById(kakoId); // 過去ログファイル
+const gtdSheet = wflFile.getSheetByName('GTD'); // 新GTD
 
 // 行
 const beginRow_MBO = 4;
+const beginRow_GTD = 11; // 新GTD（開始行）
 const endRow_MBO = mboSheet.getMaxRows();
+const endRow_GTD = gtdSheet.getMaxRows(); // 新GTD（終了行）
 const mboRow = 893;
+const gtdRow = endRow_GTD - beginRow_GTD + 1; // 新GTD（行数）※見出し行含む
 const mboRow_DayStart = 150;
 
 // 列(全シート共通)
 const endCol_MBO = mboSheet.getMaxColumns();
+const endCol_GTD = gtdSheet.getMaxColumns(); // 新GTD（列数）
 const mboStartCol = 1; // MBOシート開始列
+const gtdStartCol = 1; // GTDシート開始列
 const mboEndCol = 68; // MBOシート終了列
 const hiddenColNum = 2; // MBO_表示・非表示列（数値）
+const gtdFilterColNum = 1; // GTD_表示・非表示列（数値）
 const eventColNum = 6; // MBO_Eventエリア開始列（数値）
 const wStartColNum = 7; // MBO_Wエリア開始列（数値）
 const fStartColNum = 16; // MBO_Fエリア開始列（数値）
@@ -41,3 +48,5 @@ const prevMonthRunCell = 'E2' // 前月表示モードを実行するフラグ
 const nextMonthRunCell = 'E3' // 翌月表示モードを実行するフラグ
 const accordionRunCell = 'B3' // アコーディオンを実行するフラグ
 const kakoYearCell = 'C518' // 過去ログ対象とする年度
+const gtdCurrentMonthCheckCell = 'A6'; // GTD当月チェックボックス
+const gtdDelayCheckCell = 'B6'; // GTD遅延チェックボックス
---
+++ src/05_GTDFilter.js
@@ -0,0 +1,56 @@
+// GTDフィルタ機能
+function gtdFilter(e){
+    const gtdCurrentMonthCheckFlag = gtdSheet.getRange(gtdCurrentMonthCheckCell).getValue(); // 当月フラグ
+    const gtdDelayCheckFlag = gtdSheet.getRange(gtdDelayCheckCell).getValue(); // 遅延フラグ
+
+    // イベント対象シートがGTDシート上であることの判定
+    const eGTDSheetGet = e.source.getActiveSheet();
+    const eGTDSheetCk = eGTDSheetGet.getName() === gtdSheet.getName();
+  
+    // イベント対象セルがマクロ実行セルであることの判定
+    const eGTDRangeGet = e.range;
+    const eGTDRangeCk1 = eGTDRangeGet.getA1Notation() === gtdCurrentMonthCheckCell; // 当月チェック
+    const eGTDRangeCk2 = eGTDRangeGet.getA1Notation() === gtdDelayCheckCell; // 遅延チェック
+    const eGTDRangeCk = eGTDRangeCk1 || eGTDRangeCk2;
+
+    // 指定年度のデータに変更が発生していることの判定
+    const eGTDValueCk = e.value !== e.oldvalue;
+  
+    // 実行処理（シート・セル・中身の条件が合致した場合に実行）
+    if(eGTDSheetCk && eGTDRangeCk && eGTDValueCk){
+        let filterGTD = gtdSheet.getFilter();
+        if(filterGTD !== null){
+            gtdSheet.getFilter().remove();
+        }
+        let rule = SpreadsheetApp.newFilterCriteria()
+            .whenTextContains('')
+            .build();
+        if(eGTDRangeCk1 && eGTDRangeCk2){
+            rule = SpreadsheetApp.newFilterCriteria()
+                .whenTextContains('Y')
+                .whenTextContains('R')
+                .build();
+        }
+        if(!eGTDRangeCk1 && eGTDRangeCk2){
+            rule = SpreadsheetApp.newFilterCriteria()
+                .whenTextContains('Y')
+                .whenTextContains('R')
+                .build();
+        }
+        if(eGTDRangeCk1 && !eGTDRangeCk2){
+            rule = SpreadsheetApp.newFilterCriteria()
+                .whenTextContains('C')
+                .whenTextContains('G')
+                .whenTextContains('Y')
+                .whenTextContains('R')
+                .build();
+        }
+        if(!eGTDRangeCk1 && !eGTDRangeCk2){
+            rule = SpreadsheetApp.newFilterCriteria()
+                .whenTextContains('')
+                .build();
+        }
+        mboSheet.getRange(beginRow_GTD,gtdStartCol,gtdRow,endCol_GTD).createFilter()
+            .setColumnFilterCriteria(gtdFilterColNum,rule);
+    }
+  }

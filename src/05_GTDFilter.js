068aa64c9dfd6aa7b620325ef2f647084fee51e5
Kevin Kevinson <144573362+kevinsonz@users.noreply.github.com>
2025-04-13 11:18:57 +0900

    #11 フィルタバグ修正＋年月切替え仕様変更

--- src/05_GTDFilter.js
+++ src/05_GTDFilter.js
@@ -2,26 +2,33 @@
 function gtdFilter(e){
     // イベント対象シートがGTDシート上であることの判定
     const eGTDSheetGet = e.source.getActiveSheet();
-    const eGTDSheetCk = eGTDSheetGet.getName() === gtdSheet.getName();
+    const eGTDSheetCheck = eGTDSheetGet.getName() === gtdSheet.getName();
   
     // イベント対象セルがマクロ実行セルであることの判定
     const eGTDRangeGet = e.range;
-    const eGTDRangeCk = eGTDRangeGet.getA1Notation() === gtdFilterCheckCell; // 実行チェック
-  
-    // 指定年度のデータに変更が発生していることの判定
-    const eGTDValueCk = e.value !== e.oldvalue;
+    const eGTDRangeCheck = eGTDRangeGet.getA1Notation() === gtdFilterCheckCell; // 実行チェック
+    const runFlug1 = eGTDSheetCheck && eGTDRangeCheck;
   
     // 実行処理（シート・セル・中身の条件が合致した場合に実行）
-    if(eGTDSheetCk && eGTDRangeCk && eGTDValueCk){
+    if(runFlug1){
       let filterGTD = gtdSheet.getFilter();
       if(filterGTD !== null){
-          gtdSheet.getFilter().remove();
+        gtdSheet.getFilter().remove();
       }
-      if(e.value){
-      let rule = SpreadsheetApp.newFilterCriteria()
+
+      const nextMonth = gtdSheet.getRange(gtdImakokoMonthNextCell).getValue();
+      gtdSheet.getRange(gtdImakokoMonthCurrentCell).setValue(nextMonth);
+
+      const monthCheck = gtdSheet.getRange(gtdMonthCheckCell).getValue();
+      const priorityCheck = gtdSheet.getRange(gtdPriorityCheckCell).getValue();
+      const hiddenCheck = gtdSheet.getRange(gtdHiddenCheckCell).getValue() === "なし";
+      const runFlug2 = monthCheck || priorityCheck || !hiddenCheck;
+
+      if(runFlug2){
+        let rule = SpreadsheetApp.newFilterCriteria()
           .whenTextContains('V')
           .build();
-      gtdSheet.getRange(beginRow_GTD,gtdStartCol,gtdRow,endCol_GTD).createFilter()
+        gtdSheet.getRange(beginRow_GTD,gtdStartCol,gtdRow,endCol_GTD).createFilter()
           .setColumnFilterCriteria(gtdFilterColNum,rule);
       }
     }
